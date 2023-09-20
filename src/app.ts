395e87872b53a5daa0f20013dceea860f614124a
StartX <business.libay@gmail.com>
2023-09-20 12:13:09 -0500

    cors origin pm

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compresion());
-app.use(cors());
+app.use(cors({origin: "http://localhost:3000", credentials: true}));
 app.use(
   session({
     secret: config.sessionSecret,
