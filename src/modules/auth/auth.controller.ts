245101c20fe0c1a5b26d49e0058f331b8463066f
michaelcardoza <mc.michaelcardoza@gmail.com>
2023-09-20 06:10:23 -0500

    refactor: add oauth router

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,78 +1,24 @@
-import type { NextFunction, Request, Response } from 'express';
-import { LoginUserDto, RegisterUserDto } from '@app/modules/auth/dto';
-import {
-  createUser,
-  findUserByEmailOrThrow,
-  mapUser,
-  validateIfUserExists,
-} from '@app/modules/users/user.services';
-import { passwordMatches } from '@app/modules/auth/auth.services';
-import { generateToken } from '@app/shared/services/tokens';
-import { config } from '@app/configs/app.config';
+import type { Request, Response } from 'express';
+import passport from '@app/configs/passport.config';
 
-export const register = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const payload = <RegisterUserDto>req.body;
-
-    await validateIfUserExists(payload.email);
-
-    const user = await createUser(payload);
-
-    res.json(mapUser(user));
-  } catch (error) {
-    next(error);
-  }
+export const login = {
+  github: {
+    access: passport.authenticate('github', {
+      scope: ['user:email'],
+    }),
+    callback: passport.authenticate('github', {
+      failureRedirect: '/auth/failed',
+    }),
+    redirect: (req: Request, res: Response) => {
+      res.redirect('/');
+    },
+  },
 };
 
-export const login = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const payload = <LoginUserDto>req.body;
-    const user = await findUserByEmailOrThrow(
-      payload.email,
-      'Invalid credentials',
-    );
-
-    await passwordMatches(payload.password, user.password);
-
-    const accessToken = await generateToken(
-      { userId: user._id },
-      config.access_token_secret,
-      '1d',
-    );
-    const refreshToken = await generateToken(
-      { userId: user._id },
-      config.refresh_token_secret,
-      '30d',
-    );
-
-    res.json({
-      user: mapUser(user),
-      accessToken,
-      refreshToken,
-    });
-  } catch (error) {
-    next(error);
+export const logout = (req: Request, res: Response) => {
+  if (req.isAuthenticated()) {
+    req.logout({}, () => null);
   }
-};
 
-export const refreshToken = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    res.json({
-      message: 'Refresh Token',
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.json({});
 };
--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,14 +1,14 @@
 import express from 'express';
-import { validation } from '@app/shared/middlewares';
-import {
-  register,
-  login,
-  refreshToken,
-} from '@app/modules/auth/auth.controller';
-import { LoginUserDto, RegisterUserDto } from '@app/modules/auth/dto';
+import { login, logout } from '@app/modules/auth/auth.controller';
 
 export const AuthRouter = express.Router();
 
-AuthRouter.route('/auth/register').post(validation(RegisterUserDto), register);
-AuthRouter.route('/auth/login').post(validation(LoginUserDto), login);
-AuthRouter.route('/auth/refreshToken').post(refreshToken);
+AuthRouter.route('/auth/github').get(login.github.access);
+AuthRouter.route('/auth/github/callback').get(
+  login.github.callback,
+  login.github.redirect,
+);
+AuthRouter.route('/auth/failed').get(() => {
+  return {};
+});
+AuthRouter.route('/auth/logout').get(logout);
