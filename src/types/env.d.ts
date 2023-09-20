0846e664847d2fea208db8594e924609023ca07d
michaelcardoza <mc.michaelcardoza@gmail.com>
2023-09-20 06:05:45 -0500

    refactor: types env

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -5,9 +5,12 @@ declare global {
     interface ProcessEnv {
       NODE_ENV?: string;
       APP_PORT?: string;
+      APP_BASE_URL?: string;
+      CLIENT_URL?: string;
+      SESSION_SECRET?: string;
+      GITHUB_CLIENT_ID?: string;
+      GITHUB_CLIENT_SECRET?: string;
       DATABASE_URL?: string;
-      ACCESS_TOKEN_SECRET?: string;
-      REFRESH_TOKEN_SECRET?: string;
     }
   }
 }
